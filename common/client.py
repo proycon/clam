fad550dcfde85d062edf803cc47d871e80c09bb7
mvgompel <mvgompel@12f355fe-0486-481a-ad91-c297ab22b4e3>
2010-05-18 12:26:00 +0000

    implementing client methods
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/clam@4624 12f355fe-0486-481a-ad91-c297ab22b4e3

--- common/client.py
+++ common/client.py
@@ -17,6 +17,8 @@ from lxml import etree as ElementTree
 from StringIO import StringIO
 import codecs
 import os.path
+from httplib2 import Http
+from urllib import urlencode
 
 import clam.common.status
 import clam.common.parameters
@@ -24,11 +26,42 @@ import clam.common.formats
 
 VERSION = 0.2
 
+
 class FormatError(Exception):
          def __init__(self, value):
              self.value = value
          def __str__(self):
              return "Not a valid CLAM XML response"
+
+class BadRequest(Exception):
+         def __init__(self):
+            pass
+         def __str__(self):
+            return "Bad Request"
+
+class NotFound(Exception):
+         def __init__(self):
+            pass
+         def __str__(self):
+            return "Not Found"
+
+class PermissionDenied(Exception):
+         def __init__(self):
+            pass
+         def __str__(self):
+            return "Permission Denied"
+
+class ServerError(Exception):
+         def __init__(self):
+            pass
+         def __str__(self):
+            return "Server Error"
+
+class AuthRequired(Exception):
+         def __init__(self):
+            pass
+         def __str__(self):
+            return "Authorization Required"
 
 class CLAMFile:
     def __init__(self, path, format):
@@ -61,7 +94,9 @@ class CLAMData(object):
         self.corpora = []
         self.input = []
         self.output = []
+        self.projects = None
         self.parseresponse(xml)
+
 
     def parseresponse(self, xml):
         global VERSION
@@ -121,6 +156,11 @@ class CLAMData(object):
                             if unicode(format) == filenode.attrib['format']: #TODO: verify
                                 selectedformat = format
                         self.input.append( CLAMFile('output/' + filenode.text, selectedformat) )
+            elif node.tag == 'projects': 
+                 self.projects = []
+                 for projectnode in node:
+                    if projectnode.tag == 'projects':
+                        self.projects.append(projectnode.text)
 
     def parameter(self, id):                                 
         """Return the specified parameter"""
@@ -136,39 +176,85 @@ class CLAMData(object):
             return self.parameter(id)
         except KeyError:
             raise
+
+    def parameterdict(self):
+        """Return all parameters as id => value dictionary"""
+        paramdict = {}
+        for parametergroup, params in parameters:
+            for parameter in params:
+                if parameter.value:
+                    paramdict[parameter.id] = parameter.value
+        return paramdict
+        
  
 class CLAMAuth:
     def __init__(self, user, password):
         pass
 
+
+
 class CLAMClient:
-    def __init__(self, host, port):
-        pass
+    def __init__(self, url):
+        self.http = Http()
+        if url[-1] != '/': url += '/'
+        self.url = url
+        
+
+    def _parse(self, response, content):
+        if response['status'] == 200:
+            return CLAMData(content)
+        elif response['status'] == 400:
+            raise BadRequest()
+        elif response['status'] == 401:
+            raise AuthRequired()
+        elif response['status'] == 403:
+            raise PermissionDenied()
+        elif response['status'] == 404:
+            raise NotFound()
+                
     
-    def getproject(self, project, auth = None):
+    def index(self, auth = None):
+        """get index of projects"""
+        response, content = self.http.request(self.url)
+        return self._parse(response, content)                    
+
+    def get(self, project, auth = None):
         """query the project status"""
-        pass
+        response, content = self.http.request(self.url + project + '/')
+        return self._parse(response, content)                    
     
-    def createproject(self,project, auth = None):
-        pass
+    def create(self,project, auth = None):
+        """create a new project"""
+        response, content = self.http.request(self.url + project + '/','PUT')
+        return self._parse(response, content)               
 
-    def postproject(self, project, parameters, auth = None):
-        pass
+    def start(self, project, **parameters):
+        """start a run"""
+        auth = None
+        if 'auth' in parameters:
+            auth = parameters['auth']
+            del parameters['auth']
 
-    def abortproject(self,project, auth = None):
-        """aborts and deletes a project"""
-        pass
+        assert( isinstance(parameters, dict) )
+
+        response, content = self.http.request(self.url + project + '/','POST', urlencode(parameters)) 
+        return self._parse(response, content)                                     
+
+    def abort(self,project, auth = None):
+        """aborts AND deletes a project"""
+        response, content = self.http.request(self.url + project + '/','DELETE') 
+        return self._parse(response, content)               
 
     def downloadarchive(self, project, format = 'zip', auth = None):
         """download all output as archive"""
         pass
 
-    def downloadfile(self, project, path, auth = None):
+    def download(self, project, path, auth = None):
         """download one output file"""
         pass
 
-    def uploadfile(self, project, path, format, auth = None):
-        """upload a file"""
+    def upload(self, project, path, format, auth = None):
+        """upload a file (or archive)"""
         pass
 
 
