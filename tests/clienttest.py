83ba3ba77636b1b045c8024f90b89273a1590404
mvgompel <mvgompel@12f355fe-0486-481a-ad91-c297ab22b4e3>
2010-05-18 13:03:04 +0000

    added external 3rd party module: poster
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/clam@4626 12f355fe-0486-481a-ad91-c297ab22b4e3

--- tests/clienttest.py
+++ tests/clienttest.py
@@ -8,21 +8,36 @@ sys.path.append(sys.path[0] + '/../../')
 os.environ['PYTHONPATH'] = sys.path[0] + '/../../'
 
 from clam.common.client import *
+from clam.common.formats import *
 
 clamclient = CLAMClient('http://localhost:8080/')
 
 
 #index of all projects
-print "PROJECT INDEX"
+print "INDEX OF ALL PROJECTS"
 index = clamclient.index()
 for project in index.projects:
     print "\t" + project        
+    
 
 #creating new project
 clamclient.create('clienttest')
 
 #get new project
 data = clamclient.get('clienttest')
+
+#make a testfile
+f = open('/tmp/tst','w')
+f.write("Dit is een test.")
+f.close()
+
+#upload it
+clamclient.upload('/tmp/tst', PlainTextFormat('utf-8') )
+
+
+
+
+
 
 
 
