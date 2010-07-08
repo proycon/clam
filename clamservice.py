7eed185ac8cbb60def9bfba125be33cf160075e7
mvgompel <mvgompel@12f355fe-0486-481a-ad91-c297ab22b4e3>
2010-07-08 12:05:52 +0000

    documentation; todo; changelog updates. Ready for beta2 release?
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/clam@5354 12f355fe-0486-481a-ad91-c297ab22b4e3

--- clamservice.py
+++ clamservice.py
@@ -42,7 +42,7 @@ import clam.config.defaults as settings #will be overridden by real settings lat
 #web.wsgiserver.CherryPyWSGIServer.ssl_private_key = "path/to/ssl_private_key"
 
 
-VERSION = '0.2.8'
+VERSION = '0.2.9'
 
 DEBUG = False
     
