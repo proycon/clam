94d026ff95f01b7341b73b0b252ac198538ac937
mvgompel <mvgompel@12f355fe-0486-481a-ad91-c297ab22b4e3>
2010-12-03 12:25:07 +0000

    First set of unit tests: parameter tests (all green)
    git-svn-id: https://ilk.uvt.nl/svn/branches/clam-nextgeneration@7114 12f355fe-0486-481a-ad91-c297ab22b4e3

---
+++ tests/parametertest.py
@@ -0,0 +1,244 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+###############################################################
+# CLAM: Computational Linguistics Application Mediator
+# -- Parameter tests --
+#       by Maarten van Gompel (proycon)
+#       http://ilk.uvt.nl/~mvgompel
+#       Induction for Linguistic Knowledge Research Group
+#       Universiteit van Tilburg
+#       
+#       Licensed under GPLv3
+#
+###############################################################
+
+import unittest2
+import sys
+import os
+
+#We may need to do some path magic in order to find the clam.* imports
+sys.path.append(sys.path[0] + '/../../')
+os.environ['PYTHONPATH'] = sys.path[0] + '/../../'
+
+import clam.common.parameters
+
+
+class BooleanParameterTest(unittest2.TestCase):
+    """Boolean Parameter Test"""
+    
+    def setUp(self):
+        self.parameter = clam.common.parameters.BooleanParameter('test','test','test')
+    
+    def test_sanity(self):
+        """Boolean parameter sanity check"""
+        self.assertTrue(self.parameter.id == self.parameter.name == self.parameter.description == 'test')
+    
+    def test_set_true(self):
+        """Setting Boolean parameter to True"""
+        success = self.parameter.set(True)
+        self.assertTrue(self.parameter.error is None, self.parameter.error)
+        self.assertTrue(success and self.parameter.hasvalue and self.parameter.value is True)
+
+    def test_set_false(self):
+        """Setting Boolean parameter to False"""
+        success = self.parameter.set(False)
+        self.assertTrue(self.parameter.error is None, self.parameter.error)
+        self.assertTrue(success and self.parameter.hasvalue and self.parameter.value is False)
+        
+
+class IntegerParameterTest(unittest2.TestCase):
+    """Integer Parameter Test"""
+    
+    def setUp(self):
+        self.parameter = clam.common.parameters.IntegerParameter('test','test','test',min=1,max=10)
+    
+    def test_sanity(self):
+        """Integer parameter - sanity check"""
+        self.assertTrue(self.parameter.id == 'test')
+        self.assertTrue(self.parameter.name == self.parameter.description == 'test')
+    
+    def test_set_mininrange(self):
+        """Integer parameter - Setting to minimum value in range"""
+        success = self.parameter.set(1)
+        self.assertTrue(self.parameter.error is None, self.parameter.error)
+        self.assertTrue(self.parameter.hasvalue)
+        self.assertTrue(self.parameter.value == 1)
+        self.assertTrue(success)        
+    
+        
+        
+    def test_set_maxinrange(self):
+        """Integer parameter - Setting to maximum value in range"""
+        success = self.parameter.set(10)
+        self.assertTrue(self.parameter.error is None, self.parameter.error)
+        self.assertTrue(self.parameter.hasvalue)
+        self.assertTrue(self.parameter.value == 10 )
+        self.assertTrue(success)
+        
+        
+    def test_set_outofrange(self):
+        """Integer parameter - setting to value out of range"""
+        success = self.parameter.set(0)
+        self.assertTrue(self.parameter.error == "Number must be a whole number between 1 and 10")
+        self.assertFalse(success)
+        self.assertFalse(self.parameter.hasvalue)
+        
+class FloatParameterTest(unittest2.TestCase):
+    """Float Parameter Test"""
+    
+    def setUp(self):
+        self.parameter = clam.common.parameters.FloatParameter('test','test','test',min=0.0,max=1.0)
+    
+    def test_sanity(self):
+        """Float parameter - sanity check"""
+        self.assertTrue(self.parameter.id == 'test')
+        self.assertTrue(self.parameter.name == self.parameter.description == 'test')
+    
+    def test_set_mininrange(self):
+        """Float parameter - setting to to minimum value in range"""
+        success = self.parameter.set(0)
+        self.assertTrue(self.parameter.error is None, self.parameter.error)
+        self.assertTrue(self.parameter.hasvalue)
+        self.assertTrue(self.parameter.value == 0)
+        self.assertTrue(success)        
+
+        
+    def test_set_midrange(self):
+        """Float parameter - setting to middle value in range"""
+        success = self.parameter.set(0.5)
+        self.assertTrue(self.parameter.error is None, self.parameter.error)
+        self.assertTrue(self.parameter.hasvalue)
+        self.assertTrue(self.parameter.value == 0.5 )
+        self.assertTrue(success)
+        
+        
+    def test_set_maxinrange(self):
+        """Float parameter - setting to maximum value in range"""
+        success = self.parameter.set(1)
+        self.assertTrue(self.parameter.error is None, self.parameter.error)
+        self.assertTrue(self.parameter.hasvalue)
+        self.assertTrue(self.parameter.value == 1 )
+        self.assertTrue(success)
+        
+        
+    def test_set_outofrange(self):
+        """Float parameter - setting to value out of range"""
+        success = self.parameter.set(2)
+        self.assertTrue(self.parameter.error == "Number must be between 0.0 and 1.0")
+        self.assertFalse(success)
+        self.assertFalse(self.parameter.hasvalue)                        
+                        
+class StringParameterTest(unittest2.TestCase):
+    """String Parameter Test"""
+    
+    def setUp(self):
+        self.parameter = clam.common.parameters.StringParameter('test','test','test',maxlength=10)
+    
+    def test_sanity(self):
+        """String parameter - sanity check"""
+        self.assertTrue(self.parameter.id == 'test')
+        self.assertTrue(self.parameter.name == self.parameter.description == 'test')
+    
+    def test_set_empty(self):
+        """String parameter - setting to an empty value"""
+        success = self.parameter.set("")
+        self.assertTrue(self.parameter.error is None, self.parameter.error)
+        self.assertTrue(self.parameter.hasvalue)
+        self.assertTrue(self.parameter.value == "")
+        self.assertTrue(success)        
+    
+
+    def test_set_value(self):
+        """String parameter - setting to a valid value"""
+        success = self.parameter.set("test")
+        self.assertTrue(self.parameter.error is None, self.parameter.error)
+        self.assertTrue(self.parameter.hasvalue)
+        self.assertTrue(self.parameter.value == "test")
+        self.assertTrue(success)        
+        
+    def test_set_maxsize(self):
+        """String parameter - setting to value that is as long as it may get"""
+        success = self.parameter.set("1234567890")
+        self.assertTrue(self.parameter.error is None, self.parameter.error)
+        self.assertTrue(self.parameter.hasvalue)
+        self.assertTrue(self.parameter.value == "1234567890")
+        self.assertTrue(success)   
+        
+    def test_set_toolong(self):
+        """String parameter - Setting to value that is too long"""
+        success = self.parameter.set("1234567890A")
+        self.assertTrue(self.parameter.error == "Text too long, exceeding maximum of 10 characters allowed")
+        self.assertFalse(success)
+        self.assertFalse(self.parameter.hasvalue) 
+        
+class ChoiceParameterTest(unittest2.TestCase):
+    """Choice Parameter Test"""
+    
+    def setUp(self):
+        self.parameter = clam.common.parameters.ChoiceParameter('test','test','test',choices=[('a','A'),('b','B'),('c','C')] )
+                        
+    def test_sanity(self):
+        """Choice parameter - sanity check"""
+        self.assertTrue(self.parameter.id == 'test')
+        self.assertTrue(self.parameter.name == self.parameter.description == 'test')
+        self.assertTrue(self.parameter.multi == False)                    
+        self.assertTrue(len(self.parameter.choices) == 3)                    
+
+    def test_set_value(self):
+        """Choice parameter - setting to a valid value"""
+        success = self.parameter.set("a")
+        self.assertTrue(self.parameter.error is None, self.parameter.error)
+        self.assertTrue(self.parameter.hasvalue)
+        self.assertTrue(self.parameter.value == "a")
+        self.assertTrue(success)
+        
+    def test_set_invalid(self):
+        """Choice parameter - setting to an invalid value"""
+        success = self.parameter.set("d")
+        self.assertTrue(self.parameter.error)
+        self.assertFalse(success)
+        self.assertFalse(self.parameter.hasvalue)
+
+class MultiChoiceParameterTest(unittest2.TestCase):
+    """Choice Parameter Test"""
+    
+    def setUp(self):
+        self.parameter = clam.common.parameters.ChoiceParameter('test','test','test',choices=[('a','A'),('b','B'),('c','C')], multi=True )
+                        
+    def test_sanity(self):
+        """MultiChoice parameter - sanity check"""
+        self.assertTrue(self.parameter.id == 'test')
+        self.assertTrue(self.parameter.name == self.parameter.description == 'test')
+        self.assertTrue(self.parameter.multi == True)                    
+        self.assertTrue(len(self.parameter.choices) == 3)                    
+
+    def test_set_value(self):
+        """MultiChoice parameter - setting to a single valid value"""
+        success = self.parameter.set(["a"])
+        self.assertTrue(self.parameter.error is None, self.parameter.error)
+        self.assertTrue(self.parameter.hasvalue)
+        self.assertTrue(self.parameter.value == ["a"])
+        self.assertTrue(success)
+        
+    def test_set_valuemulti(self):
+        """MultiChoice parameter - setting to multiple valid value"""
+        success = self.parameter.set(['a','b','c'])
+        self.assertTrue(self.parameter.error is None, self.parameter.error)
+        self.assertTrue(self.parameter.hasvalue)
+        self.assertTrue(self.parameter.value == ['a','b','c'])
+        self.assertTrue(success)
+        
+    def test_set_invalid(self):
+        """MultiChoice parameter - setting to an invalid value"""
+        success = self.parameter.set("d")
+        self.assertTrue(self.parameter.error)
+        self.assertFalse(success)
+        self.assertFalse(self.parameter.hasvalue)
+                
+
+                        
+                                        
+if __name__ == '__main__':
+    unittest2.main(verbosity=2)
+    
