be7e8fc550ff8bff75c41877871ac2f1ed3b5969
mvgompel <mvgompel@12f355fe-0486-481a-ad91-c297ab22b4e3>
2010-12-06 14:39:01 +0000

    new unit tests
    git-svn-id: https://ilk.uvt.nl/svn/branches/clam-nextgeneration@7152 12f355fe-0486-481a-ad91-c297ab22b4e3

---
+++ tests/servicetest.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+###############################################################
+# CLAM: Computational Linguistics Application Mediator
+# -- CLAM Client for Text Statistics webservice --
+#       by Maarten van Gompel (proycon)
+#       http://ilk.uvt.nl/~mvgompel
+#       Induction for Linguistic Knowledge Research Group
+#       Universiteit van Tilburg
+#       
+#       Licensed under GPLv3
+#
+###############################################################
+
+import sys
+import os
+import time
+import glob
+import random
+import codecs
+import unittest2
+
+#We may need to do some path magic in order to find the clam.* imports
+
+sys.path.append(sys.path[0] + '/../../')
+os.environ['PYTHONPATH'] = sys.path[0] + '/../../'
+
+#Import the CLAM Client API and CLAM Data API and other dependencies
+from clam.common.client import *
+from clam.common.data import *
+from clam.common.formats import *
+import clam.common.status
+
+
+class BasicServiceTest(unittest2.TestCase):
+    """Test basic operations"""
+    
+    def setUp(self):
+        self.url = 'http://' + os.uname()[1] + ':8080'
+        self.client = CLAMClient(self.url)
+
+    def test1_index(self):
+        """Basic Service Test - Index and and sanity"""
+        data = self.client.index()
+        self.assertTrue(data.system_id == "textstats")
+        self.assertTrue(isinstance(data.projects,list))
+        self.assertTrue(data.profiles)
+        self.assertTrue(data.parameters)
+
+    def test2_1_create(self):
+        """Basic Service Test - Project creation"""
+        success = self.client.create('basicservicetest')
+        self.assertTrue(success)
+        
+    def test2_2_create(self):
+        """Basic Service Test - Project availability in index"""
+        data = self.client.index()
+        self.assertTrue('basicservicetest' in data.projects)
+    
+    def test2_3_create(self):
+        """Basic Service Test - Project state retrieval"""
+        data = self.client.get('basicservicetest')
+        self.assertTrue(data.system_id == "textstats")
+        self.assertTrue(data.profiles)
+        self.assertTrue(data.parameters)        
+        self.assertTrue(isinstance(data.input,list))        
+
+    def test2_4_delete(self):
+        """Basic Service Test - Project deletion"""
+        success = self.client.delete('basicservicetest')
+        self.assertTrue(success)
+        
