e012ddb965f3690abe65ffb259485712d7537bda
mvgompel <mvgompel@12f355fe-0486-481a-ad91-c297ab22b4e3>
2010-12-11 12:53:17 +0000

    Updated defaults
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/clam/master@7293 12f355fe-0486-481a-ad91-c297ab22b4e3

--- config/defaults.py
+++ config/defaults.py
@@ -13,62 +13,134 @@
 #
 ###############################################################
 
-# *** DO NOT ALTER THIS FILE, MAKE A COPY INSTEAD! ***
+
+# ******** DO NOT ALTER THIS FILE! MAKE A COPY INSTEAD ***********
 
 from clam.common.parameters import *
 from clam.common.formats import *
+from clam.common.converters import *
+from clam.common.viewers import *
+from clam.common.data import *
 from clam.common.digestauth import pwhash
+import sys
 
-REQUIRE_VERSION = 0.2
+REQUIRE_VERSION = 0.5
 
-#========================== META INFORMATION ===================================
-
+# ======== GENERAL INFORMATION ===========
 
 #The System ID, a short alphanumeric identifier for internal use only
-SYSTEM_ID = "clam"
+SYSTEM_ID = "xxx"
 
 #System name, the way the system is presented to the world
-SYSTEM_NAME = "CLAM: Computional Linguistics Application Mediator"
+SYSTEM_NAME = ""
 
 #An informative description for this system:
-SYSTEM_DESCRIPTION = "CLAM is a webservice wrapper around NLP tools"
+SYSTEM_DESCRIPTION = ""
 
-
-#========================== SERVER ADMINISTRATION ===================================
-
+# ======== LOCATION ===========
 
 #The root directory for CLAM, all project files, (input & output) and
-#pre-installed corpora will be stored here. The directories will be
-#made if they do not exist yet. Set to an absolute path.
+#pre-installed corpora will be stored here. Set to an absolute path:
 ROOT = ""
 
-#The port the webserver (either built-in or a parent server) runs on:
-PORT = 80
-#The host the webserver (either built-in or a parent server) runs on, note
-#that clients are forced to use the very same hostname, so make sure it's
-#accessible as-such.
-HOST = 'localhost'
+#The URL of the system
+PORT= 80
 
-#If CLAM runs in an existing webserver, it can be configured to run at certain deeper URL, rather than at the root of the server (don't worry about leading/trailing slashes). In a stand-alone webserver, CLAM will always run in the root.
-#URLPREFIX = ''
+#Hostname of the system
+HOST = "localhost"
+
+# ======== AUTHENTICATION & SECURITY ===========
 
 #Users and passwords
 USERS = None #no user authentication
-#USERS = { 'username': pwhash('username', SYSTEM_ID, 'secret') } #Using pwhash and plaintext password in code is not secure!! 
+#USERS = { 'admin': pwhash('admin', SYSTEM_ID, 'secret'), 'proycon': pwhash('proycon', SYSTEM_ID, 'secret'), 'antal': pwhash('antal', SYSTEM_ID, 'secret') , 'martin': pwhash('martin', SYSTEM_ID, 'secret') }
 
-ADMINS = []
-
-
-#========================== SERVICE CONFIGURATION ===================================
-
+ADMINS = ['admin'] #Define which of the above users are admins
 
 #Do you want all projects to be public to all users? Otherwise projects are 
 #private and only open to their owners and users explictly granted access.
-PROJECTS_PUBLIC = True
+PROJECTS_PUBLIC = False
+
+# ======== WEB-APPLICATION STYLING =============
+
+#Choose a style (has to be defined as a CSS file in style/ )
+STYLE = 'classic'
+
+#Amount of free memory required prior to starting a new process (in Kbytes!)
+REQUIREMEMORY = 10 * 1024
+
+
+
+# ======== PREINSTALLED DATA ===========
+
+#INPUTSOURCES = [
+#    InputSource(id='sampledocs',label='Sample texts',path=ROOT+'/inputsources/sampledata',defaultmetadata=PlainTextFormat(None, encoding='utf-8') ),
+#]
+
+# ======== PROFILE DEFINITIONS ===========
+
+PROFILES = []
+
+#Example:
+
+#PROFILES = [ 
+    #Profile(
+        #InputTemplate('textinput', PlainTextFormat,"Input text document",  
+            #StaticParameter(id='encoding',name='Encoding',description='The character encoding of the file', value='utf-8'),  
+            #ChoiceParameter(id='language',name='Language',description='The language the text is in', choices=[('en','English'),('nl','Dutch'),('fr','French')]),
+            #StringParameter(id='author',name='Author',description="The author's name", maxlength=100),
+            #IntegerParameter(id='year',name='Year of Publication',description="The year of publication", minvalue=1900,maxvalue=2030),
+            #CharEncodingConverter(id='latin1',label='Convert from Latin-1',charset='iso-8859-1'),
+            #PDFtoTextConverter(id='pdfconv',label='Convert from PDF Document'),
+            #MSWordConverter(id='docconv',label='Convert from MS Word Document'),
+            #InputSource(id='sampledoc', label="Sample Document", path=ROOT+'/inputsources/sampledoc.txt', metadata=PlainTextFormat(None, encoding='utf-8',language='en')),
+            #extension='.txt',
+            #multi=True
+        #),
+        #OutputTemplate('statsbydoc',PlainTextFormat,'Document Statistics',
+            #SetMetaField('encoding','ascii'),
+            #extension='.stats',
+            #multi=True
+        #),
+        #OutputTemplate('freqlistbydoc', PlainTextFormat,'Document Frequency list ', 
+            #CopyMetaField('language','textinput.language'), 
+            #CopyMetaField('encoding','textinput.encoding'), 
+            #SimpleTableViewer(),
+            #extension='.freqlist',
+            #multi=True
+        #),
+        #OutputTemplate('overallstats', PlainTextFormat, 'Overall Statistics',
+            #SetMetaField('encoding','ascii'),
+            #ParameterCondition(author_set=True, 
+                #then=ParameterMetaField('author','author'), 
+            #),
+            #filename='overall.stats',
+            #unique=True
+        #), 
+        #OutputTemplate('overallfreqlist', PlainTextFormat, 'Overall Frequency List',
+            #SetMetaField('encoding','utf-8'),
+            #ParameterCondition(author_set=True, 
+                #then=ParameterMetaField('author','author'), 
+            #),
+            #SimpleTableViewer(),
+            #filename='overall.freqlist',
+            #unique=True
+        #), 
+        #ParameterCondition(createlexicon=True, 
+            #then=OutputTemplate('lexicon', PlainTextFormat, 'Lexicon',
+                #SetMetaField('encoding','utf-8'),
+                #filename='overall.lexicon',
+                #unique=True
+            #)            
+        #)
+    #) 
+#]
+
+# ======== COMMAND ===========
 
 #The system command. It is recommended you set this to small wrapper
 #script around your actual system. Full shell syntax is supported. Using
-#absolute paths is always preferred. The current working directory will be
+#absolute paths is preferred. The current working directory will be
 #set to the project directory.
 #
 #You can make use of the following special variables, 
@@ -85,24 +157,21 @@ PROJECTS_PUBLIC = True
 #     $PARAMETERS      - List of chosen parameters, using the specified flags
 #
 COMMAND = ""
-
 #Example:
-#COMMAND = "/home/proycon/work/clam/tests/uctowrapper.sh $STATUSFILE $INPUTDIRECTORY $OUTPUTDIRECTORY $PARAMETERS"
+#COMMAND = "/path/to/command " + $DATAFILE $STATUSFILE $OUTPUTDIRECTORY"
 
-#The parameters are subdivided into several group. In the form of a list of (groupname, parameters) tuples. The parameters are a list of instances from common/parameters.py
+# ======== PARAMETER DEFINITIONS ===========
+
+#The parameters are subdivided into several groups. In the form of a list of (groupname, parameters) tuples. The parameters are a list of instances from common/parameters.py
 PARAMETERS = []
 
-#List of supported Input formats by the system
-#New format types should be added to common/formats.py, and can then be used here:
-INPUTFORMATS = []
-
 #Example:
-#INPUTFORMATS = [ PlainTextFormat('utf-8',['txt']) ]
-
-#List of delivered Output formats by the system (it's not mandatory for all these filetypes to be delivered at the same time)
-OUTPUTFORMATS = []
-
-#Example:
-#OUTPUTFORMATS = [ PlainTextFormat('utf-8',['txt']) ]
-
+#PARAMETERS =  [ 
+    #('Main', [ 
+       #BooleanParameter(id='createlexicon',name='Create Lexicon',description='Generate a separate overall lexicon?'),
+       #ChoiceParameter(id='casesensitive',name='Case Sensitivity',description='Enable case sensitive behaviour?', choices=['yes','no'],default='no'),
+       #IntegerParameter(id='freqlistlimit',name='Limit frequencylist',description='Limit entries in frequencylist to the top scoring ones. Value of zero (no limit) or higher',minvalue=0, maxvalue=99999999),
+       #StringParameter(id='author',name='Author',description='Sign output metadata with the specified author name',maxlength=255),
+    #] )
+#]
 
