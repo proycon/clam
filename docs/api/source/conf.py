e31bf705889430eca8e1e5f9e7218d790ca19693
mvgompel <mvgompel@12f355fe-0486-481a-ad91-c297ab22b4e3>
2010-12-11 14:07:22 +0000

    documentation fix
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/clam/master@7295 12f355fe-0486-481a-ad91-c297ab22b4e3

--- docs/api/source/conf.py
+++ docs/api/source/conf.py
@@ -22,7 +22,7 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = []
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -164,7 +164,7 @@ htmlhelp_basename = 'CLAMdoc'
 # -- Options for LaTeX output --------------------------------------------------
 
 # The paper size ('letter' or 'a4').
-#latex_paper_size = 'letter'
+latex_paper_size = 'a4'
 
 # The font size ('10pt', '11pt' or '12pt').
 #latex_font_size = '10pt'
