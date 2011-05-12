e30b894f6ac7175a70b1f7024079b9df6aa032e6
mvgompel <mvgompel@12f355fe-0486-481a-ad91-c297ab22b4e3>
2011-05-12 21:42:28 +0000

    correctly scroll to editor when it pops up
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/clam/master@9918 12f355fe-0486-481a-ad91-c297ab22b4e3

--- static/clam.js
+++ static/clam.js
@@ -123,7 +123,7 @@ $(document).ready(function(){
 
 
    //Open in-browser editor
-   $("#openeditor").click(function(event){  $("#editormask").show(); window.location.hash="editor"; $("#editor").slideDown(); })
+   $("#openeditor").click(function(event){  $("#editormask").show(); window.scroll(0,0); $("#editor").slideDown(); })
 
     
 
