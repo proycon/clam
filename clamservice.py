4554deeffba0b28eef48fd7b38bcc9dc159e7b5f
Maarten van Gompel <proycon@anaproy.nl>
2011-11-02 15:44:16 +0100

    fix in preauthheader

--- clamservice.py
+++ clamservice.py
@@ -166,7 +166,7 @@ def requirelogin(f):
         printdebug("wrapper: "+ repr(f))   
         if settings.PREAUTHHEADER:            
             printdebug("Header debug: " + repr(web.ctx.env))
-            user = web.ctx.env.get(settings.PREAUTHHEADER, '')
+            user = web.ctx.env.get('HTTP_' + settings.PREAUTHHEADER.upper(), '')
             printdebug("Got pre-authenticated user: " + user)   
             if user:
                 if settings.PREAUTHMAPPING:
