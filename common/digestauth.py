bb32fdeeecb98a576effd8021950f149440fea0d
Maarten van Gompel <proycon@anaproy.nl>
2012-03-13 13:23:45 +0100

    quotes

--- common/digestauth.py
+++ common/digestauth.py
@@ -135,16 +135,16 @@ class auth(object):
         standardsUncompliant = self.tolerateIE and ("MSIE" in web.ctx.environ['HTTP_USER_AGENT'])
         
         if reqHeaderDict['realm'] != self.realm:
-            print "DEBUG directiveProper: realm not matching got " + reqHeaderDict['realm'] + ' expected ' + self.realm
+            print "DEBUG directiveProper: realm not matching got '" + reqHeaderDict['realm'] + "' expected '" + self.realm + "'"
             return False
         elif not (standardsUncompliant or reqHeaderDict.get('opaque','') == self.opaque):
-            print "DEBUG directiveProper: got opaque " + str(reqHeaderDict.get('opaque','')) + ' expected ' + str(self.opaque)
+            print "DEBUG directiveProper: got opaque '" + str(reqHeaderDict.get('opaque','')) + "' expected '" + str(self.opaque) + '"'
             return False
         elif len(reqHeaderDict['nc']) != 8:
             print "DEBUG directiveProper nc != 8"
             return False
         elif not (reqHeaderDict['uri'] == reqPath or (standardsUncompliant and "?" in reqPath and reqPath.startswith(reqHeaderDict['uri']))): 
-            print "DEBUG mismatch in request paths, got " +  str(reqHeaderDict['uri']) + " instead of " + str(reqPath)
+            print "DEBUG mismatch in request paths, got '" +  str(reqHeaderDict['uri']) + "' instead of '" + str(reqPath) + "'"
             return False
             
         return True
