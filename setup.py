bc28c20f804dd5e02b70568f835b271d62eb5d66
Maarten van Gompel <proycon@anaproy.nl>
2012-08-24 18:11:53 +0200

    fixes in build

--- setup.py
+++ setup.py
@@ -11,9 +11,10 @@ if not os.path.exists('clam'):
     if not os.path.exists('build'): os.mkdir('build')   
     os.chdir('build')
     if not os.path.exists('clam'): os.mkdir('clam')
-    os.system('cp -Rpdf ../* clam/ 2> /dev/null')
+    os.system('cp -Rpdfv ../*py ../static ../style ../templates ../tests ../config ../common ../clients ../clampopener ../external ../wrappers ../docs clam/')
     os.system('mv -f clam/setup.py .')
-    os.system('cp -f clam/README .')  
+    os.system('cp -f clam/README clam/INSTALL clam/ChangeLog clam/COPYING .')  
+   
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
@@ -28,7 +29,7 @@ setup(
     keywords = "clam webservice rest nlp computational_linguistics rest",
     url = "http://ilk.uvt.nl/clam",
     packages=['clam','clam.common','clam.config','clam.external','clam.external.poster','clam.wrappers'],
-    long_description=read('../README'),
+    long_description=read('README'),
     classifiers=[
         "Development Status :: 4 - Beta",
         "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
