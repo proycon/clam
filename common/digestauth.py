1ad2017eebe58be1e14a2f35dc0c6e2e5f95ee3c
Maarten van Gompel <proycon@anaproy.nl>
2012-11-09 13:57:07 +0100

    update

--- common/digestauth.py
+++ common/digestauth.py
@@ -22,7 +22,7 @@ class MalformedAuthenticationHeader(Exception): pass
 
 class auth(object):
     """A decorator class implementing digest authentication (RFC 2617)"""
-    def __init__(self,  getHA1,  realm="Protected",  printdebug = None, urlprefix = None, tolerateIE = True, redirectURL = '/',  unauthHTML = None,  nonceSkip = 0, staticopaque=None, lockTime = 20,  nonceLife = 350,  tries=3,  domain=[]):
+    def __init__(self,  getHA1,  realm="Protected",  printdebug = None, urlprefix = None, tolerateIE = True, redirectURL = '/unauth',  unauthHTML = None,  nonceSkip = 0, staticopaque=None, lockTime = 20,  nonceLife = 350,  tries=3,  domain=[]):
         """Creates a decorator specific to a particular web application. 
             getHA1: a function taking the arguments (username, realm), and returning digestauth.H(username:realm:password), or
                             throwing KeyError if no such user
@@ -43,10 +43,10 @@ class auth(object):
         self.outstandingNonces = NonceMemory()
         self.user_status = {}
         if staticopaque:
-            if self.printdebug: self.printdebug("DEBUG set opaque from settings: " + str(staticopaque))
+            if self.printdebug: self.printdebug("DEBUG set opaque from settings: '" + str(staticopaque)) + "'"
             self.opaque = staticopaque
         else:
-            if self.printdebug: self.printdebug("DEBUG generated opaque: " + str(self.opaque))
+            if self.printdebug: self.printdebug("DEBUG generated opaque: '" + str(self.opaque)+ "'")  
             self.opaque = "%032x" % random.getrandbits(128)
         
         self.webpy2 = web.__version__.startswith('0.2')
