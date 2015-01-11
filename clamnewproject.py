020450d24fdf74b898edaa26dd31e4b4c3b7d81b
Maarten van Gompel <proycon@anaproy.nl>
2015-01-11 14:16:54 +0100

    update

--- clamnewproject.py
+++ clamnewproject.py
@@ -17,31 +17,34 @@
 #
 ###############################################################
 
+
+from __future__ import print_function, unicode_literals, division, absolute_import
+
 import sys
 import os
-import codecs
+import io
 import shutil
 
 try:
     import clam
 except ImportError:
-    print >>sys.stderr, "ERROR: Unable to find CLAM. Either something went wrong during installation or you did not install CLAM globally and are in a directory from which it can not be accessed."
+    print("ERROR: Unable to find CLAM. Either something went wrong during installation or you did not install CLAM globally and are in a directory from which it can not be accessed.",file=sys.stderr)
     sys.exit(2)
 
 import getopt
 
 def usage():
-        print >> sys.stderr, "Syntax: clamnewproject.py system_id [options]"
-        print >> sys.stderr, "Description: This tool sets up a new CLAM project for you. Replace 'system_id' with a short ID/name for your project, this ID is for internal use only and will be used in various filenames, no spaces or other special characters allowed."
-        print >> sys.stderr, "Options:"
-        print >> sys.stderr, "\t-d [dir]      - Directory prefix, rather than in current working directory"
-        print >> sys.stderr, "\t-f            - Force use of a directory that already exists"
-        print >> sys.stderr, "\t-h            - This help message"
-        print >> sys.stderr, "Configuration shortcuts:"
-        print >> sys.stderr, "\t-n [name]     - A human-readable name. Shortcut option, can be also set in service configuration file later."
-        print >> sys.stderr, "\t-H [hostname] - Hostname. Shortcut option, can be also set in service configuration file later. "
-        print >> sys.stderr, "\t-p [port]     - Port.  Shortcut option, can be also set in service configuration file later."
-        print >> sys.stderr, "\t-u [url]      - Force URL. Shortcut option, can be also set in service configuration file later."
+        print("Syntax: clamnewproject.py system_id [options]",file=sys.stderr)
+        print("Description: This tool sets up a new CLAM project for you. Replace 'system_id' with a short ID/name for your project, this ID is for internal use only and will be used in various filenames, no spaces or other special characters allowed.",file=sys.stderr)
+        print("Options:",file=sys.stderr)
+        print("\t-d [dir]      - Directory prefix, rather than in current working directory",file=sys.stderr)
+        print("\t-f            - Force use of a directory that already exists",file=sys.stderr)
+        print("\t-h            - This help message",file=sys.stderr)
+        print("Configuration shortcuts:",file=sys.stderr)
+        print("\t-n [name]     - A human-readable name. Shortcut option, can be also set in service configuration file later.",file=sys.stderr)
+        print("\t-H [hostname] - Hostname. Shortcut option, can be also set in service configuration file later. ",file=sys.stderr)
+        print("\t-p [port]     - Port.  Shortcut option, can be also set in service configuration file later.",file=sys.stderr)
+        print("\t-u [url]      - Force URL. Shortcut option, can be also set in service configuration file later.",file=sys.stderr)
 
 
 def main():
@@ -51,7 +54,7 @@ def main():
 
     sysid = sys.argv[1]
     if ' ' in sysid or '.' in sysid or '-' in sysid or ',' in sysid or ':' in sysid or ':' in sysid or '(' in sysid or ')' in sysid or '/' in sysid or "'" in sysid or '"' in sysid:
-        print >>sys.stderr, "Invalid characters in system ID. Only alphanumerics and underscores are allowed."
+        print("Invalid characters in system ID. Only alphanumerics and underscores are allowed.",file=sys.stderr)
         sys.exit(2)
 
 
@@ -63,9 +66,9 @@ def main():
 
     try:
         opts, args = getopt.getopt(sys.argv[2:], "hd:cH:p:u:fn:")
-    except getopt.GetoptError, err:
+    except getopt.GetoptError as err:
         # print help information and exit:
-        print str(err)
+        print(str(err))
         usage()
         sys.exit(2)
 
@@ -87,7 +90,7 @@ def main():
             name = a
         else:
             usage()
-            print "ERROR: Unknown option: ", o
+            print("ERROR: Unknown option: ", o)
             sys.exit(2)
 
 
@@ -95,17 +98,17 @@ def main():
     clampath = clam.__path__[0]
 
     if not os.path.exists(clampath + '/config/template.py') or not os.path.exists(clampath + '/wrappers/template.py'):
-        print >>sys.stderr, "ERROR: Templates not found. Unable to create new project"
+        print("ERROR: Templates not found. Unable to create new project",file=sys.stderr)
         sys.exit(2)
 
     dir = dirprefix + "/" +sysid
 
     if os.path.exists(dir):
         if not force:
-            print >>sys.stderr, "ERROR: Directory " +dir + " already exists.. Unable to make new CLAM project. Add -f (force) if you want to continue nevertheless "
+            print("ERROR: Directory " +dir + " already exists.. Unable to make new CLAM project. Add -f (force) if you want to continue nevertheless ",file=sys.stderr)
             sys.exit(2)
     else:
-        print >>sys.stderr, "Making project directory " + dir
+        print("Making project directory " + dir,file=sys.stderr)
         os.mkdir(dir)
 
     if not os.path.exists(dir+ "/__init__.py"):
@@ -113,8 +116,8 @@ def main():
         f.close()
 
     if not os.path.exists(dir + '/' + sysid + '.py'):
-        fin = codecs.open(clampath + '/config/template.py','r','utf-8')
-        fout = codecs.open(dir + '/' + sysid + '.py','w','utf-8')
+        fin = io.open(clampath + '/config/template.py','r',encoding='utf-8')
+        fout = io.open(dir + '/' + sysid + '.py','w',encoding='utf-8')
         for line in fin:
             if line == "SYSTEM_ID = \"\"\n":
                 line =  "SYSTEM_ID = \"" + sysid + "\""
@@ -137,20 +140,20 @@ def main():
         fin.close()
         fout.close()
     else:
-        print >>sys.stderr, "WARNING: Service configuration file " + dir + '/' + sysid + ".py already seems to exists, courageously refusing to overwrite"
+        print("WARNING: Service configuration file " + dir + '/' + sysid + ".py already seems to exists, courageously refusing to overwrite",file=sys.stderr)
         sys.exit(2)
 
     if not os.path.exists(dir + '/' + sysid + '-wrapper.py'):
         shutil.copyfile(clampath + '/wrappers/template.py', dir + '/' + sysid + '-wrapper.py')
-        os.chmod(dir + '/' + sysid + '-wrapper.py', 0755)
+        os.chmod(dir + '/' + sysid + '-wrapper.py', 0o755)
     else:
-        print >>sys.stderr, "WARNING: System wrapper file " + dir + '/' + sysid + '-wrapper.py already seems to exists, defiantly refusing to overwrite'
+        print("WARNING: System wrapper file " + dir + '/' + sysid + '-wrapper.py already seems to exists, defiantly refusing to overwrite',file=sys.stderr)
         sys.exit(2)
 
     s = "Your new CLAM project has been set up!\n"
     s += "WHAT'S NEXT? Now you can edit your service configuration file " +  dir + '/' + sysid + ".py and your system wrapper script " +   dir + '/' + sysid + "-wrapper.py . Consult the CLAM Documentation and/or instruction videos on http://proycon.github.com/clam for further details.\n\n"
 
-    print >>sys.stderr, s
+    print(s,file=sys.stderr)
 
     if FORCEURL:
         url = FORCEURL
@@ -165,9 +168,9 @@ def main():
         url += '/'
 
     s2 = "STARTING CLAM? Whilst you are in the process of building your CLAM webservice, you can start and test your webservice using the built-in development webserver: $ clamservice -P " + dirprefix + "/" + sysid + ' ' + sysid , " after which you can point your browser or CLAM client to " + url + ".\n\n"
-    print >>sys.stderr, s2
+    print( s2,file=sys.stderr)
 
-    print >>sys.stderr, "All of this information can be read in the " + dir + "/INSTRUCTIONS file"
+    print( "All of this information can be read in the " + dir + "/INSTRUCTIONS file",file=sys.stderr)
 
     with open(dir + "/INSTRUCTIONS",'w') as f:
         f.write(s + s2)
