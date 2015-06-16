785614dd90696c7e47728ad4946985084fde8569
Maarten van Gompel <proycon@anaproy.nl>
2015-06-16 15:28:39 +0200

    cleanup debug

--- common/client.py
+++ common/client.py
@@ -135,7 +135,6 @@ class CLAMClient:
             content = r.text
             data = self._parse(content)
             if data:
-                print(content,file=sys.stderr)
                 raise PermissionDenied(data)
             else:
                 raise PermissionDenied(content)
