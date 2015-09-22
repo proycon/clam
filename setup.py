1f7455ac6b245322a115368318c4e1c4b4535693
Maarten van Gompel <proycon@anaproy.nl>
2015-09-22 14:58:25 +0200

    further style improvements

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name = "CLAM",
-    version = "0.99.0.2",
+    version = "0.99.1.2",
     author = "Maarten van Gompel",
     author_email = "proycon@anaproy.nl",
     description = ("Computational Linguistics Application Mediator. Turn command-line NLP tools into fully-fledged RESTful webservices."),
