ccc6de707adc8934d9d30393261d12380d016f78
Maarten van Gompel <proycon@anaproy.nl>
2016-01-05 11:47:09 +0100

    python 2 fix when passing non-unicode to status.write()

--- clam/common/status.py
+++ clam/common/status.py
@@ -1,5 +1,8 @@
+from __future__ import print_function, unicode_literals, division, absolute_import
+
 import io
 import time
+import sys
 
 READY = 0
 RUNNING = 1
@@ -13,6 +16,8 @@ def write(statusfile, statusmessage, completion = 0, timestamp = False, encoding
             timestamp = int(time.time())
         if isinstance(completion, float) and completion >= 0.0 and completion <= 1.0:
             completion = int(round(completion * 100))
+        if sys.version < '3' and not isinstance(statusmessage, unicode):
+            statusmessage = unicode(statusmessage,'utf-8')
         f.write(str(completion) + "%\t" + str(timestamp) + "\t" + statusmessage + "\n")
         f.close()
 
