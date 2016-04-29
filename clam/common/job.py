ee07837e41d49ddcc518b0e6914cb30449125bb6
Maarten van Gompel <proycon@anaproy.nl>
2016-04-29 15:28:37 +0200

    update

--- clam/common/job.py
+++ clam/common/job.py
@@ -1,6 +1,6 @@
 ###############################################################
 # CLAM: Computational Linguistics Application Mediator
-# -- CLAM Data API  --
+# -- CLAM Job API  --
 #       by Maarten van Gompel (proycon)
 #       https://proycon.github.io/clam
 #
@@ -18,8 +18,8 @@ from __future__ import print_function, unicode_literals, division, absolute_impo
 import os
 import io
 import shutil
-from collections import defaultdict
 import flask #only used for rendering templates
+import subprocess
 
 import clam.common.util
 import clam.common.data
@@ -36,7 +36,7 @@ class CLAMJob(object):
     COMMAND = None
     SETTINGSMODULE = "NONE" #only needed for dispatching
 
-    def __init__(self, projectpath, settingsmodule=None, async=False):
+    def __init__(self, projectpath, settingsmodule=None):
         self.id = self.__class__.__name__
         self.projectpath = projectpath
         if self.projectpath[-1] != '/':
@@ -51,8 +51,7 @@ class CLAMJob(object):
         self.parameters = []
         self.commandlineparams = []
         self.data = None
-        self.async = async
-        self.nextseq = {} 
+        self.nextseq = {}
         if settingsmodule:
             self.SETTINGSMODULE = settingsmodule
             import_string = "import " + settingsmodule + " as localsettings"
@@ -168,7 +167,7 @@ class CLAMJob(object):
 
 
 
-    def start(self, **kwargs):
+    def __call__(self, **kwargs):
         self.errors,self.parameters, self.commandlineparams = clam.common.data.processparameters(kwargs, self.PARAMETERS)
 
         if not self.errors: #We don't even bother running the profiler if there are errors
@@ -186,79 +185,72 @@ class CLAMJob(object):
                     raise clam.common.data.ParameterError("Parameter " + parameter.id + " raised an error:" + parameter.error)
         elif not matchedprofiles:
             raise Exception("No profiles matching input and parameters, unable to start. Are you sure you added all necessary input files and set all necessary parameters?")
-        elif self.COMMAND:
-            #everything good, external call, serialize to XML file and dispatch external process
-            with io.open(os.path.join(self.projectpath,"clam.xml"),'wb') as f:
-                f.write(flask.render_template('response.xml',
-                    version=VERSION,
-                    system_id=self.__class__.__name__,
-                    system_name=self.NAME if self.NAME else self.__class__.__name__,
-                    system_description="",
-                    system_version=self.VERSION,
-                    system_email="",
-                    user="anonymous",
-                    project=os.path.basename(self.projectpath),
-                    url="",
-                    errors=self.errors,
-                    errormsg="",
-                    parameterdata=self.parameters,
-                    inputsources=None,
-                    outputpaths=[],
-                    inputpaths=clam.common.data.inputindex(self.projectpath, self.PROFILES),
-                    profiles=self.PROFILES,
-                    matchedprofiles=",".join([str(x) for x in matchedprofiles_byindex]), #comma-separated list of indices (str)
-                    program=program, #Program instance
-                    datafile=True,
-                    projects=[],
-                    actions=[],
-                    info=False
-                    ))
-            #dispatch process
-            cmd = self.COMMAND
-            cmd = cmd.replace('$PARAMETERS', " ".join(self.commandlineparams)) #commandlineparams is shell-safe
-            cmd = cmd.replace('$INPUTDIRECTORY', self.projectpath + 'input/')
-            cmd = cmd.replace('$OUTPUTDIRECTORY',self.projectpath + 'output/')
-            cmd = cmd.replace('$TMPDIRECTORY',self.projectpath + 'tmp/')
-            cmd = cmd.replace('$STATUSFILE',self.projectpath + '.status')
-            cmd = cmd.replace('$DATAFILE',self.projectpath + 'clam.xml')
-            cmd = cmd.replace('$USERNAME',"anonymous")
-            cmd = cmd.replace('$PROJECT',os.path.basename(self.projectpath)) #alphanumberic only, shell-safe
-            cmd = cmd.replace('$OAUTH_ACCESS_TOKEN','')
-            cmd = clam.common.data.escapeshelloperators(cmd)
-            #everything should be shell-safe now
-            cmd += " 2> " + self.projectpath + "output/error.log" #add error output
+        else:
+            clamdataxml = flask.render_template('response.xml',
+                version=VERSION,
+                system_id=self.__class__.__name__,
+                system_name=self.NAME if self.NAME else self.__class__.__name__,
+                system_description="",
+                system_version=self.VERSION,
+                system_email="",
+                user="anonymous",
+                project=os.path.basename(self.projectpath),
+                url="",
+                errors=self.errors,
+                errormsg="",
+                parameterdata=self.parameters,
+                inputsources=None,
+                outputpaths=[],
+                inputpaths=clam.common.data.inputindex(self.projectpath, self.PROFILES),
+                profiles=self.PROFILES,
+                matchedprofiles=",".join([str(x) for x in matchedprofiles_byindex]), #comma-separated list of indices (str)
+                program=program, #Program instance
+                datafile=True,
+                projects=[],
+                actions=[],
+                info=False
+            )
 
-            cmd = DISPATCHER + ' . ' + self.SETTINGSMODULE + ' ' + self.projectpath + ' ' + cmd
-            printlog("Starting dispatcher " +  DISPATCHER + " with " + self.COMMAND + ": " + repr(cmd) + " ..." )
-            process = subprocess.Popen(cmd,cwd=self.projectpath, shell=True)
-            if process:
-                pid = process.pid
-                printlog("Started dispatcher with pid " + str(pid) )
-                with open(self.projectpath + '.pid','w') as f: #will be handled by dispatcher!
-                    f.write(str(pid))
-                if not self.async:
+            if self.COMMAND:
+                #everything good, external call, serialize to XML file and dispatch external process
+                with io.open(os.path.join(self.projectpath,"clam.xml"),'wb') as f:
+                    f.write(clamdataxml)
+                #dispatch process
+                cmd = self.COMMAND
+                cmd = cmd.replace('$PARAMETERS', " ".join(self.commandlineparams)) #commandlineparams is shell-safe
+                cmd = cmd.replace('$INPUTDIRECTORY', self.projectpath + 'input/')
+                cmd = cmd.replace('$OUTPUTDIRECTORY',self.projectpath + 'output/')
+                cmd = cmd.replace('$TMPDIRECTORY',self.projectpath + 'tmp/')
+                cmd = cmd.replace('$STATUSFILE',self.projectpath + '.status')
+                cmd = cmd.replace('$DATAFILE',self.projectpath + 'clam.xml')
+                cmd = cmd.replace('$USERNAME',"anonymous")
+                cmd = cmd.replace('$PROJECT',os.path.basename(self.projectpath)) #alphanumeric only, shell-safe
+                cmd = cmd.replace('$OAUTH_ACCESS_TOKEN','')
+                cmd = clam.common.data.escapeshelloperators(cmd)
+                #everything should be shell-safe now
+                cmd += " 2> " + self.projectpath + "output/error.log" #add error output
+
+                cmd = DISPATCHER + ' . ' + self.SETTINGSMODULE + ' ' + self.projectpath + ' ' + cmd
+                #printlog("Starting dispatcher " +  DISPATCHER + " with " + self.COMMAND + ": " + repr(cmd) + " ..." )
+                process = subprocess.Popen(cmd,cwd=self.projectpath, shell=True)
+                if process:
+                    pid = process.pid
+                    #printlog("Started dispatcher with pid " + str(pid) )
+                    with open(self.projectpath + '.pid','w') as f: #will be handled by dispatcher!
+                        f.write(str(pid))
                     self.returncode = process.wait()
                     return self.returncode
                 else:
-                    return None
+                    raise Exception("Unable to launch process")
             else:
-                raise Exception("Unable to launch process")
-        else:
-            #everything good, no external call, prepare data object
-            self.run(data)
-
-        self.done()
-
-
-    def __call__(self): #alias
-        self.start()
-
-    def done(self):
-        pass
+                #everything good, no external call, prepare data object
+                return self.run(clam.common.data.CLAMData(clamdataxml))
 
     def run(self, data):
+        """Called when the job has to run, should be overloaded, will be called when __call__ is invoked"""
         raise NotImplementedError("run() method should be overloaded")
 
     def clean(self):
-       shutil.rmtree(self.projectpath)
+        """Deletes all files for the job"""
+        shutil.rmtree(self.projectpath)
 
