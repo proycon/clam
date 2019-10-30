ae8875915fe725cc17add2829be3d0b484b09f3b
Maarten van Gompel <proycon@anaproy.nl>
2019-10-30 15:18:44 +0100

    another version bump

--- clam/common/data.py
+++ clam/common/data.py
@@ -34,7 +34,7 @@ import clam.common.status
 import clam.common.util
 import clam.common.viewers
 
-VERSION = '3.0.2'
+VERSION = '3.0.3'
 
 #dirs for services shipped with CLAM itself
 CONFIGDIR = os.path.abspath(os.path.dirname(__file__) + '/../config/')
