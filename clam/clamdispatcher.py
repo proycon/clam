proycon/clam

commit 2ecf8e56efedfa5709dbbfc77a55d57de92754fa

oauth: redirect back to error_callback when the oauthsession returned an error (such as an expired token)

clam/common/auth.py
@@ -437,7 +437,15 @@ class OAuth2(HTTPAuth):
                 else:
                     oauthsession = OAuth2Session(self.client_id, token={'access_token': oauth_access_token, 'token_type': 'bearer'})
                     if self.userinfo_url: oauthsession.USERINFO_URL = self.userinfo_url
-                    username = self.username_function(oauthsession)
+                    if oauthsession.get('error'):
+                        #happens for instance when the token is expired
+                        self.printdebug("OAuth Session returned an error:" + str(oauthsession.get('error',"")) + " - " + str(oauthsession.get('error_description',"")))
+                        return self.auth_error_callback()
+                    try:
+                        username = self.username_function(oauthsession)
+                    except clam.common.oauth.OAuthError as e:
+                        self.printdebug("Could not obtain username from OAuth session")
+                        return self.auth_error_callback()
                     if username:
                         #add (username, oauth_access_token) tuple as parameter to the wrapped function
                         kwargs['credentials'] =  (username, oauth_access_token)

commit 926f0dbffa8edb408748a895f2de96e9c196f234

fixed regression introduced in 2ecf8e56efedfa5709dbbfc77a55d57de92754fa: fallback when token expires (oauthsession is not a dictionary)

clam/clamdispatcher.py
@@ -24,7 +24,7 @@ import signal
 import shutil
 import json
 
-VERSION = '3.1.6'
+VERSION = '3.1.8'
 
 sys.path.append(sys.path[0] + '/..')
 

clam/common/auth.py
@@ -437,14 +437,10 @@ class OAuth2(HTTPAuth):
                 else:
                     oauthsession = OAuth2Session(self.client_id, token={'access_token': oauth_access_token, 'token_type': 'bearer'})
                     if self.userinfo_url: oauthsession.USERINFO_URL = self.userinfo_url
-                    if oauthsession.get('error'):
-                        #happens for instance when the token is expired
-                        self.printdebug("OAuth Session returned an error:" + str(oauthsession.get('error',"")) + " - " + str(oauthsession.get('error_description',"")))
-                        return self.auth_error_callback()
                     try:
                         username = self.username_function(oauthsession)
                     except clam.common.oauth.OAuthError as e:
-                        self.printdebug("Could not obtain username from OAuth session")
+                        self.printdebug("Could not obtain username from OAuth session, got OAuthError error " + str(e))
                         return self.auth_error_callback()
                     if username:
                         #add (username, oauth_access_token) tuple as parameter to the wrapped function

clam/common/data.py
@@ -38,7 +38,7 @@ import clam.common.status
 import clam.common.util
 import clam.common.viewers
 
-VERSION = '3.1.7'
+VERSION = '3.1.8'
 
 #dirs for services shipped with CLAM itself
 CONFIGDIR = os.path.abspath(os.path.dirname(__file__) + '/../config/')

setup.py
@@ -26,7 +26,7 @@ def read(fname):
 
 setup(
     name = "CLAM",
-    version = "3.1.7", #also change in clam.common.data.VERSION and dispatcher.py and codemeta.json
+    version = "3.1.8", #also change in clam.common.data.VERSION and dispatcher.py and codemeta.json
     author = "Maarten van Gompel",
     author_email = "proycon@anaproy.nl",
     description = ("Turns command-line tools into fully-fledged RESTful webservices with an auto-generated web-interface for human end-users."),