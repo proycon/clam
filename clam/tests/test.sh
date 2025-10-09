7da771e1251b940e1fc8108c40d1aca9a059788d
Maarten van Gompel <proycon@anaproy.nl>
2025-10-09 21:45:22 +0200

    better test feedback on failure

--- clam/tests/test.sh
+++ clam/tests/test.sh
@@ -10,20 +10,24 @@ HOSTNAME=$(hostname)
 echo "Checking installation..." >&2
 
 GOOD=1
+FAILMSG=""
 
 if ! command -v clamservice 2>/dev/null; then
    echo "ERROR: clamservice not found (run this script in a virtual environment with clam installed)" >&2
+   FAILMSG="$FAILMSG clamservice"
    GOOD=0
 fi
 
 if ! command -v clamnewproject 2>/dev/null; then
    echo "ERROR: clamnewproject not found" >&2
+   FAILMSG="$FAILMSG clamnewproject"
    GOOD=0
 fi
 
 cd /tmp || exit 2
 if ! clamnewproject -f clamnewprojecttest --noninteractive; then
    echo "ERROR: clamnewproject didn't run well" >&2
+   FAILMSG="$FAILMSG clamnewproject"
    GOOD=0
 fi
 rm -Rf clamnewprojecttest 2> /dev/null
@@ -31,11 +35,13 @@ cd -
 
 if ! command -v clamdispatcher 2>/dev/null; then
    echo "ERROR: clamdispatcher not found" >&2
+   FAILMSG="$FAILMSG clamdispatcher"
    GOOD=0
 fi
 
 if ! command -v clamclient 2>/dev/null; then
    echo "ERROR: clamclient not found" >&2
+   FAILMSG="$FAILMSG clamclient"
    GOOD=0
 fi
 
@@ -44,17 +50,19 @@ echo "  ..ok" >&2
 echo "Running parameter tests:" >&2
 if ! python parametertest.py; then
    echo "ERROR: Parameter test failed!!" >&2
+   FAILMSG="$FAILMSG parametertest"
    GOOD=0
 fi
 
 echo "Running data tests:" >&2
 if ! python datatest.py; then
    echo "ERROR: Data test failed!!" >&2
+   FAILMSG="$FAILMSG datatest"
    GOOD=0
 fi
 
 echo "Stopping all running clam services" >&2
-kill $(ps aux | grep 'clamservice' | awk '{print $2}') 2>/dev/null
+pkill -f clamservice
 sleep 2
 
 echo "Starting clam textstats service" >&2
@@ -65,13 +73,14 @@ echo "Running service tests:" >&2
 if ! python servicetest.py; then
    echo "ERROR: Service test failed!!" >&2
    GOOD=0
+   FAILMSG="$FAILMSG servicetest"
    echo "<--------------------- servicetest.server.log --------------------------------->" >&2
    cat servicetest.server.log >&2
    echo "</-------------------- servicetest.server.log --------------------------------->" >&2
 fi
 
 echo "Stopping clam service" >&2
-kill $(ps aux | grep 'clamservice' | awk '{print $2}') 2>/dev/null
+pkill -f clamservice
 
 
 echo "Starting clam service 'authtest'" >&2
@@ -83,13 +92,14 @@ echo "Running authentication tests:" >&2
 if ! python authtest.py; then
    echo "ERROR: Authentication test failed!!" >&2
    GOOD=0
+   FAILMSG="$FAILMSG authtest"
    echo "<--------------------- authtest.server.log --------------------------------->" >&2
    cat authtest.server.log >&2
    echo "</-------------------- authtest.server.log --------------------------------->" >&2
 fi
 
 echo "Stopping clam service" >&2
-kill $(ps aux | grep 'clamservice' | awk '{print $2}') 2>/dev/null
+pkill -f clamservice
 sleep 2
 
 echo "Starting clam service 'forwardauthtest'" >&2
@@ -102,6 +112,7 @@ if ! curl -f -H "X-Custom-User: test" "http://$HOSTNAME:8080/"; then
    echo "<--------------------- forwardauthtest.server.log --------------------------------->" >&2
    cat forwardauthtest.server.log >&2
    echo "</-------------------- forwardauthtest.server.log --------------------------------->" >&2
+   FAILMSG="$FAILMSG forwardauthtest"
    GOOD=0
 fi
 
@@ -110,11 +121,12 @@ if curl -f "http://$HOSTNAME:8080/"; then
    echo "<--------------------- forwardauthtest.server.log --------------------------------->" >&2
    cat forwardauthtest.server.log >&2
    echo "</-------------------- forwardauthtest.server.log --------------------------------->" >&2
+   FAILMSG="$FAILMSG forwardauthtest"
    GOOD=0
 fi
 
 echo "Stopping clam service" >&2
-kill "$(ps aux | grep 'clamservice' | awk '{print $2}')" 2>/dev/null
+pkill -f clamservice
 sleep 2
 
 echo "Starting clam service 'actiontest'" >&2
@@ -125,6 +137,7 @@ echo "Running actions tests:" >&2
 if ! python actiontest.py; then
    echo "ERROR: Action test failed!!" >&2
    GOOD=0
+   FAILMSG="$FAILMSG actiontest"
    echo "<--------------------- actiontest.server.log --------------------------------->" >&2
    cat actiontest.server.log >&2
    echo "</-------------------- actiontest.server.log --------------------------------->" >&2
@@ -142,6 +155,7 @@ echo "Running actions tests:" >&2
 if ! python authactiontest.py; then
    echo "ERROR: AuthAction test failed!!" >&2
    GOOD=0
+   FAILMSG="$FAILMSG authactiontest"
    echo "<--------------------- authactiontest.server.log --------------------------------->" >&2
    cat authactiontest.server.log >&2
    echo "</-------------------- authactiontest.server.log --------------------------------->" >&2
@@ -158,6 +172,7 @@ sleep 5
 echo "Running constraint tests:" >&2
 if ! python constrainttest.py; then
    echo "ERROR: constraint test failed!!" >&2
+   FAILMSG="$FAILMSG constrainttest"
    GOOD=0
    echo "<--------------------- constrainttest.server.log --------------------------------->" >&2
    cat constrainttest.server.log >&2
@@ -172,7 +187,7 @@ if [ $GOOD -eq 1 ]; then
     echo "Done, all tests passed!" >&2
     exit 0
 else
-    echo "TESTS FAILED!!!!" >&2
+    echo "TESTS FAILED!!!! $FAILMSG" >&2
     exit 1
 fi
 
