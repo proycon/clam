d7a684dafc8dd7afc1d408f1f1b452033d113caf
Maarten van Gompel <proycon@anaproy.nl>
2026-07-21 14:15:42 +0200

    tests: initial setup

--- Makefile
+++ Makefile
@@ -4,3 +4,12 @@ all:
 
 plan.pdf: PLAN.md
 	pandoc $< --pdf-engine=xelatex -o $@
+
+.PHONY: test
+test:
+	-if [ -e .pid ]; then kill `cat .pid`; fi
+	if [ -e .pid ]; then rm .pid; fi
+	cargo run -- --config tests/testservice/test.toml & echo $$! > .pid
+	echo "(2s grace period for service to start)">&2 && sleep 2
+	hurl --test --verbose --jobs 1 tests/test.hurl
+	kill `cat .pid` && rm .pid
---
+++ tests/testservice/testwrapper.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ "$1" = "-t" ]; then
+    echo "starting (action)...">&2
+    #string passed (CLAM action)
+    echo "$2" | tr "[:lower:]" "[:upper:]"
+    ret=$?
+else
+    echo "starting (project)...">&2
+    #file passed (CLAM project)
+    tr "[:lower:]" "[:upper:]" < "$1" > "$(basename "$1")"
+    ret=$?
+fi
+
+if [ $ret = 0 ]; then
+    echo "done...">&2
+else
+    echo "failed...">&2
+fi
+exit $ret
